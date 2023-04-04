4818db87898200bd752bb110c65df841a4db3ff8
Michael O'Donnell <michael.jon.odonnell@bristol.ac.uk>
2023-04-04 20:05:56 +0100

    Create Python forumla translator
    - Take in traverser output and AST and generate python functions
    - Can generate SUM and AVERAGE statements using code replacement
    - IF statement generates function code that is then called to return the true/false solutions to the if statement
    - Nested IF statements handed via recursive calling and creation of if function code. Final line returns a call to top most function. No pre-evaluation performed.

---
+++ reeevr/converters/pythonconverter/formula.py
@@ -0,0 +1,65 @@
+from reeevr.parsers.traverse import TraverseTree
+
+
+class PythonTransform(TraverseTree):
+    """
+    Class that deals explicitly in Excel -> Python
+    transformations only.
+    """
+
+
+    def __init__(self,  excelast, sheet, coordinate):
+
+        super().__init__(excelast, sheet, coordinate)
+        self.function_transformations = {"IF(": self.IF,
+                                         "SUM(": self.SUM,
+                                         "AVERAGE(": self.AVERAGE}
+
+    def IF(self, params):
+
+        expressionname = f"{self.outputvarname}_if_{self.depth}_{self.count}"
+        simplesyntax = self.walk(params)
+
+        if_state = "".join(simplesyntax)
+        if_state = if_state[:-1]
+        if_state = if_state.split('%sep%')
+
+        indent = '    '
+        code = f"def {expressionname}():\n" \
+               f"{indent}if {if_state[0]}:\n" \
+               f"{indent}{indent}return {if_state[1]}\n" \
+               f"{indent}else:\n" \
+               f"{indent}{indent}return {if_state[2]}\n"
+
+        self.code += code
+
+        return f'{expressionname}()'
+
+    def SUM(self, params):
+        simplesyntax = self.walk(params)
+        return f"math.sum({''.join(simplesyntax)}"
+
+    def AVERAGE(self, params):
+        """
+        Arithmetic mean
+        """
+        simplesyntax = self.walk(params)
+        return f"numpy.mean({''.join(simplesyntax)}"
+
+
+if __name__ == "__main__":
+
+    from openpyxl.formula import Tokenizer
+    from reeevr.parsers.excelast import ExcelAST
+    tokenlist = Tokenizer('= 1 + IF(IF(sheet10!A1 = "yes", AVERAGE(A10:A20), 23), SUM(B10:V20),50) + '
+                          'IF(IF(OMG!A1 = "yes", SUM(A10:A20), 70),"shit",\'My stuff\'!A1) +20 + SUM(A10:A20)')
+
+    excelAST = ExcelAST(tokenlist)
+    import jsonpickle
+
+    serialized = jsonpickle.encode(excelAST.AST)
+    #print(json.dumps(json.loads(serialized), indent=4))
+
+    test = PythonTransform(excelAST.AST, "Sheet10", "C10")
+    test.walk(test.tree)
+    print(test.code)
