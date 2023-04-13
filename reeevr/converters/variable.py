63f2be4bad15b4d26138e9535e5288d140e7fb20
Michael O'Donnell <michael.jon.odonnell@bristol.ac.uk>
2023-04-13 19:53:43 +0100

    Remove all static methods
    This allows defined names to be accessed everywhere easily

--- reeevr/converters/variable.py
+++ reeevr/converters/variable.py
@@ -1,12 +1,17 @@
 from openpyxl.utils.cell import cols_from_range
+from openpyxl.workbook.defined_name import DefinedName
 class VariableConverter:
     """
     Convert the Excel variables following a
     standard method for continuity
     """
 
-    @staticmethod
-    def excel_cell_to_variable(sheet, coordinate):
+    def __init__(self,workbook):
+        self.definednames = {}
+        self.get_defined_names(workbook)
+
+
+    def excel_cell_to_variable(self,sheet, coordinate):
         """
         Takes the Excel sheet and coordinate of a cell
         and return an equivalent name.
@@ -14,15 +19,44 @@ class VariableConverter:
         :param coordinate: coordinates of cell within sheet
         :return: programatic variable name
         """
-        sheetname = str(sheet).strip()
-        sheetname = sheetname.replace(" ", "")
-        coordinate = coordinate.replace("$", "")
-        varname = f'{sheetname}_{coordinate}'
-        varname = varname.replace("'","")
-        return varname
 
-    @staticmethod
-    def defined_name_comparison(definednames, variable):
+        if coordinate in self.definednames:
+            return coordinate
+        else:
+            sheetname = str(sheet).strip()
+            sheetname = sheetname.replace(" ", "")
+            coordinate = coordinate.replace("$", "")
+            varname = f'{sheetname}_{coordinate}'
+            varname = varname.replace("'","")
+
+        return self.defined_name_comparison(varname)
+
+    def get_defined_names(self,workbook):
+        """
+        Aquire all global defined names
+        for comparison and replacement purposes
+        """
+        try:
+            for definedName in workbook.defined_names.definedName:
+                cellName = str(definedName.attr_text).split("!")
+
+                cellLocation = self.excel_cell_to_variable(cellName[0], cellName[1])
+
+                self.definednames[f'{definedName.name}'] = f'{cellLocation}'
+
+        except AttributeError:
+            for definedName in workbook.defined_names.items():
+                cellName = str(definedName[1].attr_text).split("!")
+
+                cellLocation = self.excel_cell_to_variable(cellName[0], cellName[1])
+
+                self.definednames[f'{definedName[1].name}'] = f'{cellLocation}'
+
+        except:
+            raise
+
+
+    def defined_name_comparison(self, variable):
         """
         Checks if variable is in the defined name list.
         If it is, returns the defined name instead
@@ -32,16 +66,16 @@ class VariableConverter:
         :return: Either the programatic name, or the defined name.
         """
 
-        if variable in definednames.values():
+        if variable in self.definednames.values():
 
-            definedname = str([key for key, value in definednames.items() if variable == value])
+            definedname = str([key for key, value in self.definednames.items() if variable == value])
             return definedname
 
         else:
             return variable
 
-    @staticmethod
-    def excel_range_to_list(sheet, rangecoordinates):
+
+    def excel_range_to_list(self,sheet, rangecoordinates):
         """
         Convert an Excel range, eg A3:E10, into a
         list containing all variables. Returns both a
@@ -56,28 +90,31 @@ class VariableConverter:
         celllist = cols_from_range(rangecoordinates)
         for row in celllist:
             for variable in row:
-                list_of_variables.append(VariableConverter.excel_cell_to_variable(sheet,variable))
+                list_of_variables.append(self.excel_cell_to_variable(sheet,variable))
 
         code = str(list_of_variables)
         code = code.replace("'", "")
         return code, list_of_variables
 
-    @staticmethod
-    def variable_string_literal(sheet, cell):
 
-        varname = VariableConverter.excel_cell_to_variable(sheet,cell.coordinate)
+    def variable_string_literal(self,sheet, cell):
+
+        varname = self.excel_cell_to_variable(sheet,cell.coordinate)
 
         return {varname :[f"\'{cell.value}\'",[]]}
 
-    @staticmethod
-    def variable_numeric_literal(sheet,cell):
 
-        varname = VariableConverter.excel_cell_to_variable(sheet, cell.coordinate)
+    def variable_numeric_literal(self,sheet,cell):
+
+        varname = self.excel_cell_to_variable(sheet, cell.coordinate)
 
         return {varname :[cell.value,[]]}
 
 
 if __name__ == "__main__":
+    import openpyxl
     sheet = "sheet 1"
     range = "A1"
-    print(VariableConverter.excel_range_to_list(sheet,range))
+    wb = openpyxl.load_workbook("C:/Users/mieha/Documents/REEVER/Test workbooks/test_workbook_3.xlsx", keep_vba=True)
+    test = VariableConverter(wb)
+    print(test.excel_range_to_list(sheet,range))
