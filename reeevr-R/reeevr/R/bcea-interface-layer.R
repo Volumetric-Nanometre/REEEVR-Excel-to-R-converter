2076d35469f8d31a36f5fd4be2a5eec8f5505a40
Michael O'Donnell <michael.jon.odonnell@bristol.ac.uk>
2023-07-03 15:20:32 +0100

    Add Rudimentry BCEA interface layer
    - Add a matrix creation function
    - Add function to form BCEA outputs

--- reeevr-R/reeevr/R/bcea-interface-layer.R
+++ reeevr-R/reeevr/R/bcea-interface-layer.R
@@ -1,3 +1,7 @@
+library(BCEA)
+library(ggplot2)
+library(purrr)
+
 #' Form BCEA Dataframe
 #'
 #' Take N inputs and dumps them to screen
@@ -15,3 +19,59 @@ BCEA_dataframe <- function(...) {
 
   return(df)
 }
+
+#' Form BCEA matrix
+#'
+#' Take N inputs and dumps them to screen
+#' @param ... the N inputs
+#' @return dataframe containing all inputs
+#'
+#' @examples
+#' costs <- BCEA_matrix(a=c(1,2,3),b=c(4,5,6))
+#' effectiveness <- BCEA_matrix(a=c(1,2,3),b=c(4,5,6))
+#'
+#' @export
+
+BCEA_matrix <- function(...) {
+
+  m <- cbind(...)
+
+  return(m)
+}
+
+
+#' Call BCEA
+#'
+#' Take N inputs and dumps them to screen
+#' @param ... the N inputs
+#' @return dataframe containing all inputs
+#'
+#' @examples
+#' BCEA_all_output_loop(costs,effectiveness,treatments,Kmax)
+#'
+#' @export
+
+BCEA_all_output_loop <- function(costs,effectiveness,treatments,Kmax) {
+
+  outputpath <- "C:/Users/mo14776/Documents/tests"
+
+  totalRef <- length(treatments)
+
+
+  for(ref in 1:totalRef){
+
+    outfolder <- file.path(outputpath, treatments[ref])
+    if(!dir.exists(outfolder))
+      dir.create(outfolder)
+
+    bcea_output <- bcea(effectiveness,costs, ref = ref,interventions = treatments, Kmax = Kmax)
+    png(file.path(outfolder,"plots.png"))
+    plot(bcea_output)
+    dev.off()
+
+  }
+}
+
+
+
+
