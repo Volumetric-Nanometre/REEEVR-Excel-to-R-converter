955e0b8c53b2316abec275f1f67063ffaa456fcb
Michael O'Donnell <michael.jon.odonnell@bristol.ac.uk>
2024-02-26 09:58:11 +0000

    Add utility functions for R

--- reeevr-R/reeevr/R/utility-functions.R
+++ reeevr-R/reeevr/R/utility-functions.R
@@ -22,3 +22,56 @@ is.blank <- function(x, false.triggers=FALSE){
       (false.triggers && all(!x))
   )
 }
+
+
+#' array_construct function
+#'
+#' Correctly interprets and constructs arrays
+#' @param ... vars to form the array
+#' @param dims dimensions of the array we are constructing
+#' @return the constructed array
+#'
+#' @examples
+#' array_construct(array(c(40,40,40,40,20,20),c(2,3)),array(c(20,20,20,20,20,20),c(2,3)))
+#' @export
+#'
+array_construct <- function(...){
+
+  arraycomponents = lapply(list(...),dim)
+  print(arraycomponents)
+
+  if(any(unlist(Map(is.null, arraycomponents)))){
+
+    for(index in 1:length(arraycomponents)){
+      if(is.null(arraycomponents[[index]]))
+        arraycomponents[[index]] = length(list(...)[[index]])
+    }
+
+  }
+  print(arraycomponents)
+
+
+  if(length(unique(arraycomponents)) == 1)
+    return(array_equal(...))
+
+  else
+    return(array_unequal(...))
+
+}
+
+array_equal <- function(...){
+  print("Equal")
+
+  equalarrays = list(...)
+  innerdims = dim(equalarrays[[1]])
+  numofarrays = length(equalarrays)
+  print(numofarrays)
+  newdim =c(innerdims,numofarrays)
+  print(newdim)
+  return(array(c(...),dim = newdim))
+}
+
+array_unequal <- function(...){
+  print("Unequal")
+  return()
+}
