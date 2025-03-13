068500d90b76acf82d8a1b291b5f682c8eb71532
Michael O'Donnell <michael.jon.odonnell@bristol.ac.uk>
2025-03-13 16:48:11 +0000

    Update UI to allow preloading

--- reeevr/interactive/guiqt.py
+++ reeevr/interactive/guiqt.py
@@ -4,6 +4,7 @@ from mainloop import MainLoop
 
 from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog
 from PyQt6.QtCore import Qt
+from PyQt6 import QtGui
 from QtDesignerGUIFile import Ui_ExcelToRConverterGUI
 
 
@@ -11,12 +12,17 @@ class GUI(QMainWindow):
     def __init__(self):
         super().__init__()
 
+
         # use the Ui_login_form
         self.ui = Ui_ExcelToRConverterGUI()
         self.ui.setupUi(self)
 
+        #self.setWindowIcon(QtGui.QIcon('C:\\Users\mo14776\\Downloads\\piqrayLyT.png'))
+        self.setWindowTitle("REEEVR - Excel to R Converter")
+
         self.ui.OpenFileButton.clicked.connect(self.OpenFile)
         self.ui.OpenFolderButton.clicked.connect(self.OpenFolder)
+        self.ui.PreloadFileOpen.clicked.connect(self.OpenPreloadFile)
         self.ui.ConvertButton.clicked.connect(self.Convert)
         self.ui.progressBar.setProperty("value", 0)
         self.ui.progressBar.setAlignment(Qt.AlignmentFlag["AlignHCenter"])
@@ -27,6 +33,30 @@ class GUI(QMainWindow):
                                           "}")
         # show the login window
         self.show()
+
+    def OpenPreloadFile(self):
+
+        path = QFileDialog.getOpenFileName(parent=self,caption="Select preload file",filter="(*.txt)")
+        self.ui.PreloadInputsFile.setText(f"{path[0]}")
+
+        preload = []
+        with open(f"{path[0]}","r") as f:
+
+            for line in f:
+                try:
+                    preload.append(line.split(": ")[1].replace("\n",""))
+                    print(preload[-1])
+                except:
+                    preload.append("")
+            self.ui.FileName.setText(f"{preload[0]}")
+            self.ui.OutputFolderFileName.setText(f"{preload[1]}")
+            self.ui.OutputcellsLedit.setText(f"{preload[2]}")
+            self.ui.IgnoredSheetsLedit.setText(f"{preload[3]}")
+            self.ui.CostsCellsLEdit.setText(f"{preload[4]}")
+            self.ui.EffectivenesCellsLEdit.setText(f"{preload[5]}")
+            self.ui.TreatmentNamesLEdit.setText(f"{preload[6]}")
+            self.ui.WillingnessToPayLEdit.setText(f"{preload[7]}")
+
 
     def OpenFile(self):
 
