0c40dde346fc18dc0a494f1771a9bd4cbc1b2a4e
Michael O'Donnell <michael.jon.odonnell@bristol.ac.uk>
2025-03-17 21:43:45 +0000

    Updated to use new demo

--- reeevr/interactive/terminal.py
+++ reeevr/interactive/terminal.py
@@ -12,7 +12,8 @@ except:
 
 #path = "../../tests/test workbooks/test_workbook_4.xlsm"
 #path = "C:/Users/mo14776/OneDrive - University of Bristol/Documents/Health Economics/REEVER/Examples/Hip replacement surgery Markov model V1.0 - modified.xlsm"
-path = "C:/Users/mo14776/OneDrive - University of Bristol/Documents/Health Economics/REEVER/Examples/Hip replacement surgery Markov model V1.0.xlsm"
+#path = "C:/Users/mo14776/OneDrive - University of Bristol/Documents/Health Economics/REEVER/Examples/Hip replacement surgery Markov model V1.0.xlsm"
+path = "C:/Users/mo14776/OneDrive - University of Bristol/Documents/Health Economics/REEVER/Examples/HIPS Markov model demo.xlsm"
 
 
 #path = "C:\\Users\\mo14776\\Downloads\\DAP65 Cost-effectiveness model v6.0.xlsm"
@@ -29,7 +30,7 @@ testOutput =[('Summary results', 'F6:G9')]#[('Results','E12:L14')] #[('Summary r
 costs =[('Summary results', 'F6:F9')]#[('Results','E12:E14')] #[('Summary results', 'F6'),('Summary results', 'F7'),('Summary results', 'F8'),('Summary results', 'F9')]#[('Summary results', 'D6'),('Summary results', 'D7'),('Summary results', 'D8'),('Summary results', 'D9')] #[('Engine', 'E5'), ('Engine', 'F5')] #[('Results', 'F12'), ('Results', 'F13')]
 effs = [('Summary results', 'G6:G9')]#[('Results','G12:G14')] #[('Summary results', 'G6'),('Summary results', 'G7'),('Summary results', 'G8'),('Summary results', 'G9')]#[('Summary results', 'E6'),('Summary results', 'E7'),('Summary results', 'E8'),('Summary results', 'E9')]#[('Engine', 'E6'), ('Engine', 'F6')]#[('Results', 'G12'), ('Results', 'G13')]
 treatmentNames =['Cemented','Uncemented','Hybrid', 'Reverse Hybrid'] #['Drug A', 'Drug B','No treatment'] #['Genedrive','Genomadix cube','Laboratory genetic test', 'No test']# #['Aspirin', 'Warfarin'] #['Drug A', 'Drug B']
-willingnessToPay =[('Results','E8')]#[('Setup and run','D14')] #[('Setup and run','D20')]# #[('Model settings','N13')]
+willingnessToPay =[('Setup and run','D14')]#[('Results','E8')] #[('Setup and run','D20')]# #[('Model settings','N13')]
 
 ignoredsheets = ['DSA', 'PSA', 'PSA results', 'DSA results']#, 'State trace - Cemented', 'State trace - Hybrid', 'State trace - Reverse hybrid','State trace - Uncemented']
 print("[SUCCESS]")
@@ -46,7 +47,10 @@ print("Read code ... ",end="")
 a.read()
 print("[SUCCESS]")
 print("Generate unordered code ... ",end="")
-b = CodeGen(a.unorderedcode, outputs, codefile="test_output.R")
+b = CodeGen(varconverter, a.unorderedcode, outputs, codefile="test_output.R")
+print("[SUCCESS]")
+print("Perform second pass ... ",end="")
+b.second_pass()
 print("[SUCCESS]")
 print("Order Code ... ",end="")
 b.order_code_snippets()
