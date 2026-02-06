30e42b1f65f5eeb256d714e235f14dc07183695e
Michael O'Donnell <michael.jon.odonnell@bristol.ac.uk>
2026-02-06 12:24:17 +0000

    Add tests for "Two states Markov model_0.2.xlsm"

--- reeevr/workbooktest.py
+++ reeevr/workbooktest.py
@@ -322,16 +322,125 @@ class TestWorkbook4(unittest.TestCase):
         print(avg_inc_effs)
         print(avg_inc_nb)
 
+        # Assume statistical accuracy to 1%
         self.assertAlmostEqual(-1425.659922, avg_inc_costs/avg_inc_effs,
                                msg=f"Frontend_E9 ... [FAIL]\n"
                                    f"- Potentially failed due to random sampling.\n"
                                    f"Run at least 3 times if it failed.\n "
-                                   f"If fails 2 out of 3 times check program.",delta = 20)
+                                   f"If fails 2 out of 3 times check program.",delta = abs(-1425.659922)*0.01)
         self.assertAlmostEqual(-30965.96106, avg_inc_nb,
                                msg=f"Frontend_E9 ... [FAIL]\n"
                                    f"- Potentially failed due to random sampling.\n"
                                    f"Run at least 3 times if it failed.\n "
-                                   f"If fails 2 out of 3 times check program.", delta = 100)
+                                   f"If fails 2 out of 3 times check program.", delta = abs(-30965.96106)*0.01)
+
+    @classmethod
+    def tearDownClass(self):
+        robjects.globalenv.clear()
+
+
+class TestTwoStateMarkovV2(unittest.TestCase):
+    """
+
+    """
+    @classmethod
+    def setUpClass(self):
+        """
+        Test suite explicity loads an Excel file containing all functions and test cases.
+        File is then converted using the standard conversion process (minus culling).
+        File is then ran in R using rpy2, this generates an active R environment.
+        We then interrogate the R environment and compare to the expected value stored in Excel.
+        """
+        self.excelpath = "test/excel workbook/Two states Markov model_v0.2.xlsm"
+        self.rpath = "test/excel workbook/Two states Markov model_v0.2_output.R"
+        workbook = openpyxl.load_workbook(self.excelpath)
+        outputLang = 'R'
+        ignoredsheets = ["PSA"]
+        varconverter = VariableConverter(workbook, ignoredsheets, outputLang)
+
+        costs = [('Results', 'F12:F13')]
+        effs = [('Results', 'G12:G13')]
+        otherPSA = [('Results', 'H12:H13')]
+        outputs = ROutputs(varconverter, "test/excel workbook/", otherPSA, costs, effs,
+                           ["Drug A", "Drug B"], [('Model settings','N13')], BCEA=False)
+        reader = ExcelReader(varconverter, workbook, outputLang, ignoredsheets)
+        reader.read()
+        codegen = CodeGen(varconverter, reader.unorderedcode, outputs, "test/excel workbook/", "Two states Markov model_v0.2_output.R")
+        codegen.second_pass()
+        codegen.order_code_snippets()
+        codegen.cyclic_prune()
+        codegen.generate_code(writeoutputs=True)
+
+        with open(self.rpath,"r+") as f:
+            file_data = f.read()
+            file_data = re.sub("numberOfRuns = 1000", "numberOfRuns = 100000", file_data)
+            file_data = re.sub("converter_validate = TRUE", "converter_validate = FALSE", file_data)
+            f.seek(0)
+            f.write(file_data)
+            f.truncate()
+
+        self.r_source = robjects.r['source']
+        self.r_source(self.rpath)
+        self.globalenv = robjects.globalenv
+
+        self.psaData = np.loadtxt("test/excel workbook/psa.txt", skiprows=1)
+        os.remove("test/excel workbook/psa.txt")
+
+    def test_deterministic_conversion(self):
+        """
+        Given no changes to the Excel file, returned files should always be the same.
+        We can test this using a cryptographic hash. If the contents of the files are different then the conversion
+        has not generated the same file.
+        """
+
+        hashOriginal = file_hash("test/excel workbook/Two states Markov model_v0.2.R")
+        hashGenerated = file_hash(self.rpath)
+        self.assertEqual(hashOriginal,hashGenerated, "Hash comparison failed - converter update has changed deterministic output\n")
+
+    def test_programatic_regression(self):
+        """
+        While the converted file may be correct, the individual functions called may change definition when called in R
+        or via incompatible changes from Excel. This test catches when these regressions may occur
+        """
+
+        avgResultsF12, avgResultsF13, avgResultsG12, avgResultsG13,avgResultsH12, avgResultsH13 = np.average(self.psaData, axis=0)
+
+        # Assume statistical accuracy to 1%
+        self.assertAlmostEqual(8132.14, avgResultsF12,
+                               msg=f"Frontend_E9 ... [FAIL]\n"
+                                   f"- Potentially failed due to random sampling.\n"
+                                   f"Run at least 3 times if it failed.\n "
+                                   f"If fails 2 out of 3 times check program.", delta=8132.14 * 0.01)
+
+        self.assertAlmostEqual(6867.66, avgResultsF13,
+                               msg=f"Frontend_E9 ... [FAIL]\n"
+                                   f"- Potentially failed due to random sampling.\n"
+                                   f"Run at least 3 times if it failed.\n "
+                                   f"If fails 2 out of 3 times check program.", delta=6867.66 * 0.01)
+
+        self.assertAlmostEqual(7.59, avgResultsG12,
+                               msg=f"Frontend_E9 ... [FAIL]\n"
+                                   f"- Potentially failed due to random sampling.\n"
+                                   f"Run at least 3 times if it failed.\n "
+                                   f"If fails 2 out of 3 times check program.", delta=7.59 * 0.01)
+
+        self.assertAlmostEqual(6.96, avgResultsG13,
+                               msg=f"Frontend_E9 ... [FAIL]\n"
+                                   f"- Potentially failed due to random sampling.\n"
+                                   f"Run at least 3 times if it failed.\n "
+                                   f"If fails 2 out of 3 times check program.", delta=6.96* 0.01)
+
+        self.assertAlmostEqual(143915.57, avgResultsH12,
+                               msg=f"Frontend_E9 ... [FAIL]\n"
+                                   f"- Potentially failed due to random sampling.\n"
+                                   f"Run at least 3 times if it failed.\n "
+                                   f"If fails 2 out of 3 times check program.", delta = 143915.57*0.01)
+
+        self.assertAlmostEqual(131413.43, avgResultsH13,
+                               msg=f"Frontend_E9 ... [FAIL]\n"
+                                   f"- Potentially failed due to random sampling.\n"
+                                   f"Run at least 3 times if it failed.\n "
+                                   f"If fails 2 out of 3 times check program.", delta=131413.43*0.01)
 
     @classmethod
     def tearDownClass(self):
